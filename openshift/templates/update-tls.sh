044542996bb9382c260b87e3941b5fad7d16c06f
Daniel P. Berrange <berrange@redhat.com>
2018-01-16 18:37:45 +0000

    Fix resource name in TLS script
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- openshift/templates/update-tls.sh
+++ openshift/templates/update-tls.sh
@@ -11,6 +11,6 @@ test -f "tls-key.pem" || die "Missing tls-key.pem"
 TLS_CERT=`cat tls-cert.pem`
 TLS_KEY=`cat tls-key.pem`
 
-oc process -f virttools-planet-tls.json | oc delete -f -
-oc process -p TLS_CERT="$TLS_CERT" -p TLS_KEY="$TLS_KEY" -f virttools-planet-tls.json | oc create -f -
+oc process -f libvirt-wiki-tls.json | oc delete -f -
+oc process -p TLS_CERT="$TLS_CERT" -p TLS_KEY="$TLS_KEY" -f libvirt-wiki-tls.json | oc create -f -
 
