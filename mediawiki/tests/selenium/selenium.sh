a2eb73e6be20bab631714319e8350be3a9835aad
Peter Krempa <pkrempa@redhat.com>
2023-02-14 17:03:43 +0100

    Remove all of the old wiki tooling
    The wiki will in next steps be replaced by statically generated pages
    which can be edited via gitlab's editor.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>

--- mediawiki/tests/selenium/selenium.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-chromedriver --url-base=/wd/hub --port=4444 &
-# Make sure it is killed to prevent file descriptors leak
-function kill_chromedriver() {
-    killall chromedriver > /dev/null
-}
-trap kill_chromedriver EXIT
-npm run selenium-test
